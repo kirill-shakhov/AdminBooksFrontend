95e0f4c05e853e641adff17ade4e010dba368852
кирилл <shakhov-work@yandex.ru>
2024-01-18 13:01:30 +0300

    master feat(App) init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+  <router-link :to="{ name: 'register' }">Регистрация</router-link>
+</div>
+  <router-view/>
+</template>
+
+<script setup lang="ts">
+import {RouterView} from 'vue-router'
+</script>
+
+<style scoped>
+</style>
