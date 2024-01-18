cede488cddd5a796ae47970800c0caf48f36bf49
кирилл <shakhov-work@yandex.ru>
2024-01-18 13:03:30 +0300

    master feat(UiField) init component

---
+++ src/components/UiField/UIField.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <label v-if="label" class="block text-sm font-medium leading-6 text-gray-900">{{ label }}</label>
+
+    <div class="mt-2">
+      <slot/>
+    </div>
+  </div>
+
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  label?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  label: '',
+});
+</script>
