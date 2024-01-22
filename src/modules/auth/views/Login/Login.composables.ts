9c3cb687db50a38a52bffe0fdb7c6a2a252a5798
кирилл <shakhov-work@yandex.ru>
2024-01-22 17:22:35 +0300

    master feat(Login) add page with logic

---
+++ src/modules/auth/views/Login/Login.composables.ts
@@ -0,0 +1,66 @@
+import { computed, reactive, ref, watch } from "vue";
+import { useRouter } from 'vue-router';
+
+
+import AuthService from "../../services/AuthService.ts";
+import { object, string } from "yup";
+import { useForm } from "vee-validate";
+import { LoginTypes } from "./Login.types.ts";
+
+
+
+export function loginComposables() {
+    const router = useRouter();
+
+    let data = reactive<LoginTypes>({
+        username: '',
+        password: '',
+    })
+
+    const loading = ref<boolean>(false)
+    const status = ref<'initial' | 'pending' | 'success' | 'fail'>('initial');
+
+    const schema = object().shape({
+            username: string().required('username обязателен'),
+            password: string().required('password обязателен'),
+        }
+    );
+
+    const { handleSubmit, validate, errors, setFieldError } = useForm({
+        validationSchema: schema
+    });
+
+    async function login() {
+        try {
+            status.value = 'pending';
+            loading.value = true;
+
+            const response = await AuthService.login(data.username, data.password);
+            console.log(response);
+            localStorage.setItem('token', response.accessToken);
+            await router.push('/dashboard')
+
+            status.value = 'success';
+            loading.value = false;
+        } catch (e) {
+            console.log(e);
+
+            status.value = 'fail';
+            loading.value = false;
+        }
+
+    }
+
+    const onSubmit = handleSubmit(async (values) => {
+        await login();
+    })
+
+
+    return {
+        data,
+        loading,
+        status,
+
+        onSubmit
+    }
+}
---
+++ src/modules/auth/views/Login/Login.vue
@@ -0,0 +1,78 @@
+<!--
+  This example requires some changes to your config:
+
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+<template>
+  <!--
+    This example requires updating your template:
+
+    ```
+    <html class="h-full bg-white">
+    <body class="h-full">
+    ```
+  -->
+  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+           alt="Your Company"/>
+      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Login</h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" action="#" method="POST" @submit.prevent='onSubmit'>
+
+
+        <UiInput
+            v-model:value="data.username"
+            label="Username"
+            name="username"
+            type="text"
+        />
+
+        <UiInput
+            v-model:value="data.password"
+            label="password"
+            name="password"
+            type="text"
+        />
+
+        <div>
+          <ui-button
+              type="submit"
+          >
+            login
+          </ui-button>
+
+        </div>
+      </form>
+
+      <p class="mt-10 text-center text-sm text-gray-500">
+        Not a member?
+        {{ ' ' }}
+        <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import UiInput from "../../../../components/UiInput/UiInput.vue";
+import UiButton from "../../../../components/UiButton/UiButton.vue";
+
+import { loginComposables } from "./Login.composables.ts";
+
+const { data, onSubmit } = loginComposables()
+
+</script>
+
+<style></style>
