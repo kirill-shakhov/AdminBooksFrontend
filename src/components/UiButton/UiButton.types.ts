520015281a76b86d25200e66187f08398668cd6c
кирилл <shakhov-work@yandex.ru>
2024-01-22 12:26:25 +0300

    master feat( UiButton.vue ) added variant loading

--- src/components/UiButton/UiButton.types.ts
+++ src/components/UiButton/UiButton.types.ts
@@ -1,5 +1,6 @@
 export interface UiButtonProps {
     href?: string,
     disabled?: boolean,
-    type?: string
+    type?: string,
+    loading?: boolean
 }
--- src/components/UiButton/UiButton.vue
+++ src/components/UiButton/UiButton.vue
@@ -4,9 +4,14 @@
       :href="href"
       :disabled="disabled"
       :type="type"
+      :loading="loading"
       :class="rootClasses"
-      class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      class="flex w-full justify-center items-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
   >
+    <div v-if="loading">
+        <ui-progress-circular/>
+    </div>
+
     <slot/>
 
   </component>
@@ -15,13 +20,15 @@
 <script setup lang="ts">
 import { UiButtonProps } from "./UiButton.types.ts";
 import { UiButtonComposables } from './UiButton.composables.ts';
+import UiProgressCircular from "../UiProgressCircular/UiProgressCircular.vue";
 
 const props = withDefaults(defineProps<UiButtonProps>(), {
   disabled: false,
-  type: 'button'
+  type: 'button',
+  loading: false,
 })
 
-const {rootClasses, tag} = UiButtonComposables(props);
+const { rootClasses, tag } = UiButtonComposables(props);
 
 </script>
 
