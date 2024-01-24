719a6cbac84ed6f1ebb3c616325f83ec9f351215
кирилл <shakhov-work@yandex.ru>
2024-01-24 11:47:39 +0300

    master feat( createAxiosInstance.ts ) added interceptor for taking access token

--- src/modules/auth/services/helpers/createAxiosInstance.ts
+++ src/modules/auth/services/helpers/createAxiosInstance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { AuthResponse } from "../../models/response/AuthResponse.ts";
 
 export const API_URL = `http://localhost:5000`
 
@@ -8,8 +9,28 @@ const $api = axios.create({
 })
 
 $api.interceptors.request.use((config) => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+    if (localStorage.getItem('token')) config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
     return config;
+})
+
+$api.interceptors.response.use((config) => {
+    return config;
+}, async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status == 401 && error.config && !error.config._isRetry) {
+        originalRequest._isRetry = true;
+
+        try {
+            const response = await axios.get<AuthResponse>(`${API_URL}/auth/refresh`, { withCredentials: true })
+            localStorage.setItem('token', response.data.accessToken);
+            return $api.request(originalRequest);
+        } catch (e) {
+            console.log('не авторизован');
+            localStorage.removeItem('token');
+        }
+    }
+    throw error;
 })
 
 export default $api;
