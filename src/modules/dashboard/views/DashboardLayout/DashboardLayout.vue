65f0bc0014ee6806e2ecfcd26fe05105c7bd08bc
кирилл <shakhov-work@yandex.ru>
2024-01-24 12:21:36 +0300

    master feat( DashboardLayout.vue ) added page

---
+++ src/modules/dashboard/views/DashboardLayout/DashboardLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Header/>
+
+    Dashboard
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from "../../../../shared/components/Header/Header.vue";
+import { onMounted } from 'vue';
+
+import { useAuth } from '../../../../shared/composables/useAuth/useAuth.ts'
+const { user } = useAuth();
+
+</script>
