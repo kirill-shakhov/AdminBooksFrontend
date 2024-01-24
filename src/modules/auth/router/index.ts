2375c4aefab69ca5b2719f9dbabed3bbfe006cef
кирилл <shakhov-work@yandex.ru>
2024-01-24 12:17:44 +0300

    master feat( Auth/router ) added route for login and meta

--- src/modules/auth/router/index.ts
+++ src/modules/auth/router/index.ts
@@ -2,6 +2,13 @@ export default [
     {
         path: '/register',
         name: 'register',
-        component: () => import('../views/Registration/Registration.vue')
+        component: () => import('../views/Registration/Registration.vue'),
+        meta: { requiresAuth: false }
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('../views/Login/Login.vue'),
+        meta: { requiresAuth: false }
     }
 ]
