8ef1919dee1c3858312fd9fddcd19c2950513fff
кирилл <shakhov-work@yandex.ru>
2024-02-08 14:33:31 +0300

    master feat( DashboardLayout.vue ): added slider with books

--- src/modules/dashboard/views/DashboardLayout/DashboardLayout.vue
+++ src/modules/dashboard/views/DashboardLayout/DashboardLayout.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen">
     <Header/>
-    <div class="mx-auto max-w-7xl flex gap-5 p-6 lg:px-8">
-      Dashboard
-    </div>
+    <main class="flex-grow overflow-hidden">
+      <div class="mx-auto max-w-7xl  gap-5 p-6 lg:px-8">
+        <user-books-showcase/>
+      </div>
+    </main>
+    <Footer/>
   </div>
 </template>
 
 <script setup lang="ts">
 import Header from "../../../../shared/components/Header/Header.vue";
+import Footer from "../../../../shared/components/Footer/Footer.vue";
+import UserBooksShowcase from "../../components/UserBooksShowcase/UserBooksShowcase.vue";
 </script>
