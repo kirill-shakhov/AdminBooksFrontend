90e5f7a245ec738c0d2758233fb908fbf8a67a30
кирилл <shakhov-work@yandex.ru>
2024-02-08 21:46:31 +0300

    master feat( Registration ): added route-link to login

--- src/modules/auth/views/Registration/Registration.vue
+++ src/modules/auth/views/Registration/Registration.vue
@@ -70,9 +70,10 @@
       </form>
 
       <p class="mt-10 text-center text-sm text-gray-500">
-        Not a member?
+        Already have an account?
         {{ ' ' }}
-        <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+        <router-link :to="{name: 'login'}" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Log in!
+        </router-link>
       </p>
     </div>
   </div>
