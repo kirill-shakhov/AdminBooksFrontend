e692368a5300c68ba754bd5b053f538488493970
кирилл <shakhov-work@yandex.ru>
2024-02-22 15:13:11 +0300

    master feat( BookDetailsSkeleton.vue ) added component

---
+++ src/modules/book/views/BookDetails/BookDetailsSkeleton/BookDetailsSkeleton.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="overflow-hidden bg-white w-full py-11 animate-pulse">
+    <div
+        class="max-w-6xl px-4 py-4 mx-auto lg:py-8 md:px-6">
+      <div class="flex flex-wrap -mx-4">
+        <div class="w-full px-4 md:w-1/2 ">
+          <div class="overflow-hidden">
+            <div class="relative mb-6 lg:mb-10 lg:h-2/4">
+              <div class="object-cover bg-gray-300 w-full h-72"></div>
+            </div>
+          </div>
+        </div>
+        <div class="w-full px-4 md:w-1/2 ">
+          <div class="lg:pl-20">
+            <div class="mb-8 ">
+              <div class="flex justify-between">
+                <div class="text-lg font-medium text-rose-500 dark:text-rose-200 w-16">
+                  <div class="h-3 bg-gray-300 rounded"></div>
+                </div>
+              </div>
+              <div class="max-w-xl mt-2 mb-6 text-2xl font-bold md:text-4xl">
+                <div class="h-3 bg-gray-300 rounded"></div>
+              </div>
+              <div class="max-w-md text-gray-700 dark:text-gray-400">
+                <div class="flex-1 space-y-6 py-1">
+                  <div class="space-y-3">
+                    <div class="grid grid-cols-3 gap-4">
+                      <div class="h-3 bg-gray-300 rounded col-span-1"></div>
+                      <div class="h-3 bg-gray-300 rounded col-span-2"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="max-w-md text-gray-700 dark:text-gray-400">
+                <div class="flex-1 space-y-6 py-1">
+                  <div class="space-y-3">
+                    <div class="grid grid-cols-3 gap-4">
+                      <div class="h-3 bg-gray-300 rounded col-span-1"></div>
+                      <div class="h-3 bg-gray-300 rounded col-span-2"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+            <div class="flex flex-wrap items-center -mx-4">
+              <div class="w-full px-4 mb-4 lg:w-1/2 lg:mb-4 order-1 lg:order-1">
+                <div class="bg-gray-300 h-9 rounded-md"></div>
+              </div>
+
+              <div class="w-full px-4 mb-4 lg:mb-4 lg:w-1/2 order-3 lg:order-2">
+                <div class="bg-gray-300 h-9 rounded-md"></div>
+              </div>
+
+              <div class="w-full px-4 mb-4 lg:mb-4 order-2 lg:order-3">
+                <div class="bg-gray-300 h-9 rounded-md"></div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
