2e236c40e8ba5f53b233d6c536ce64270230f944
кирилл <shakhov-work@yandex.ru>
2024-02-23 23:28:45 +0300

    master feat( Header ) added Logo

--- src/shared/components/Header/Header.vue
+++ src/shared/components/Header/Header.vue
@@ -13,7 +13,9 @@
 
         <router-link :to="{ name: 'dashboard' }" class="-m-1.5 p-1.5 hidden lg:block">
           <span class="sr-only">Your Company</span>
-          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/>
+          <Logo
+              style="width: 60px"
+          />
         </router-link>
 
         <header-profile class="lg:hidden"/>
@@ -38,9 +40,9 @@
           class="fixed inset-y-0 left-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
         <div class="flex items-center justify-between">
           <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">Your Company</span>
-
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/>
+            <Logo
+                style="width: 60px"
+            />
           </a>
 
           <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
@@ -101,6 +103,7 @@ import {
   Bars3Icon,
   XMarkIcon,
 } from '@heroicons/vue/24/outline'
+import Logo from "../Logo/Logo.vue";
 
 const mobileMenuOpen = ref(false)
 
