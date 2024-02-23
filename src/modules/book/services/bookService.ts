f46de57ac2ca3aa13ec4c12f06cef30238cf0b00
кирилл <shakhov-work@yandex.ru>
2024-02-23 12:38:39 +0300

    master feat( BookUpload.composables.ts ) added requests for getting authors and genres

--- src/modules/book/services/bookService.ts
+++ src/modules/book/services/bookService.ts
@@ -1,22 +1,30 @@
 import $api from "../../../services/api/helpers/createAxiosInstance.ts";
-import { Book, BooksResponse, UploadBookResponse } from "../types";
+import { AuthorsResponse, Book, BooksResponse, GenresResponse, UploadBookResponse } from "../types";
 import { AxiosResponse } from "axios";
 
 class BookService {
     async getUserBooks(): Promise<BooksResponse> {
-        return $api.get<BooksResponse>(`/books`).then((response:AxiosResponse<BooksResponse>) => response.data)
+        return $api.get<BooksResponse>(`/books`).then((response: AxiosResponse<BooksResponse>) => response.data)
     }
 
     async uploadNewBook(request: FormData) {
-        return $api.post('/books/upload', request).then((response:AxiosResponse<UploadBookResponse>) => response.data);
+        return $api.post('/books/upload', request).then((response: AxiosResponse<UploadBookResponse>) => response.data);
     }
 
     async getBook(id: string): Promise<Book> {
         return $api.get<Book>(`/books/${id}`).then(response => response.data);
     }
 
-    async deleteBook(id: string): Promise<string>  {
+    async deleteBook(id: string): Promise<string> {
         return $api.delete(`/books/${id}`).then(response => response.data);
+    }
+
+    async getGenres(): Promise<GenresResponse> {
+        return $api.get(`/books/genres`).then((response: AxiosResponse<GenresResponse>) => response.data);
+    }
+
+    async getAuthors(): Promise<AuthorsResponse> {
+        return $api.get(`/books/authors`).then((response: AxiosResponse<AuthorsResponse>) => response.data);
     }
 
 }
