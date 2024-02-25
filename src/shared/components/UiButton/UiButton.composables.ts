639cf28cbb258790d3db65b7a9a0222e5aec9452
кирилл <shakhov-work@yandex.ru>
2024-02-25 13:21:10 +0300

    master fix( UiButton.composables.ts ) fixed ui of theme 'secondary'

--- src/shared/components/UiButton/UiButton.composables.ts
+++ src/shared/components/UiButton/UiButton.composables.ts
@@ -9,7 +9,7 @@ export function UiButtonComposables(props: UiButtonProps) {
             'w-full': props.block,
             'text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900"': props.theme === 'danger',
             'bg-indigo-600 text-white hover:bg-indigo-500 focus-visible:outline-indigo-600': props.theme === 'primary',
-            'font-semibold leading-6 text-gray-500 border': props.theme === 'secondary',
+            'font-semibold  ring-1 ring-slate-700/10 hover:bg-slate-50': props.theme === 'secondary',
             'text-sm shadow-sm rounded-md': props.size === 'sm',
             'text-md shadow-md rounded-lg px-6 py-3': props.size === 'md',
             'text-lg shadow-lg rounded-xl px-12 py-6': props.size === 'lg',
