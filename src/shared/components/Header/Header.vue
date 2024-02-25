61eff67232cb48922aa63c1d07065d8f7f6c6a5d
кирилл <shakhov-work@yandex.ru>
2024-02-25 13:03:50 +0300

    master fix( Header.vue ) fixed bug with focusTrap

--- src/shared/components/Header/Header.vue
+++ src/shared/components/Header/Header.vue
@@ -34,7 +34,7 @@
       </div>
 
     </nav>
-    <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" @click="" :open="mobileMenuOpen">
+    <Dialog as="div" class="lg:hidden" v-if="mobileMenuOpen" @close="mobileMenuOpen = false" @click="" :open="mobileMenuOpen">
       <div class="fixed inset-0 z-10"/>
       <DialogPanel
           class="fixed inset-y-0 left-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
@@ -47,10 +47,10 @@
 
           <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
             <span class="sr-only">Close menu</span>
-
             <XMarkIcon class="h-6 w-6" aria-hidden="true"/>
           </button>
         </div>
+        <div tabindex="0" class="sr-only">Focusable element</div>
         <div class="mt-6 flow-root">
           <div class="-my-6 divide-y divide-gray-500/10">
             <div class="space-y-2 py-6">
