4c7674fe701d8bbb3b55123620a983407e9d78cc
кирилл <shakhov-work@yandex.ru>
2024-03-07 13:12:27 +0300

    master fix( DashboardLayout ) updated imports

--- src/modules/dashboard/views/DashboardLayout/DashboardLayout.vue
+++ src/modules/dashboard/views/DashboardLayout/DashboardLayout.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="flex flex-col min-h-screen">
-    <Header/>
+    <app-header/>
     <main class="flex-grow overflow-hidden">
       <div class="mx-auto max-w-7xl  gap-5 p-6 lg:px-8">
         <user-books-showcase/>
       </div>
     </main>
-    <Footer/>
+    <app-footer/>
   </div>
 </template>
 
 <script setup lang="ts">
-import Header from "../../../../shared/components/Header/Header.vue";
-import Footer from "../../../../shared/components/Footer/Footer.vue";
-import UserBooksShowcase from "../../components/UserBooksShowcase/UserBooksShowcase.vue";
+import { AppHeader } from "@/shared/components/AppHeader";
+import { AppFooter } from "@/shared/components/AppFooter";
+import { UserBooksShowcase } from "@/modules/dashboard/components/UserBooksShowcase";
 </script>
